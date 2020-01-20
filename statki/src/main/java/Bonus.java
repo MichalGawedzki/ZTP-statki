ad679dde2dd4ea8e3fc52eccf72fb90c93462859
Jo <49189136+joannamaria27@users.noreply.github.com>
2020-01-20 21:08:03 +0100

    add Bonus.java

--- statki/src/main/java/Bonus.java
+++ statki/src/main/java/Bonus.java
@@ -1,4 +1,104 @@
-public class Bonus {
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+
+public abstract class Bonus implements IShip{
+
+
+    protected IShip iShip;
+    public Bonus(IShip iShip)
+    {
+        this.iShip=iShip;
+    }
+    @Override
+    public void spawn() {
+
+    iShip.spawn();
+
+    }
+    @Override
+    public void moveLeft() {
+
+        iShip.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+
+        iShip.moveRight();
+    }
+
+
+
+    @Override
+    public void getInstance() {
+
+        iShip.getInstance();
+    }
+
+
+
+    @Override
+    public Node getView() {
+        return iShip.getView();
+    }
+
+    @Override
+    public void setVelocity(double x, double y) {
+
+        iShip.setVelocity(x,y);
+    }
+
+    @Override
+    public double getxVelocity() {
+        return iShip.getxVelocity();
+    }
+
+    @Override
+    public void setxVelocity(double x) {
+
+        iShip.setxVelocity(x);
+    }
+
+    @Override
+    public int getHP() {
+        return iShip.getHP();
+    }
+
+    @Override
+    public void setHP(int HP) {
+
+        iShip.setHP(HP);
+    }
+
+    @Override
+    public IBullet getBullet() {
+        return iShip.getBullet();
+    }
+
+    @Override
+    public int getWeaponLevel() {
+        return iShip.getWeaponLevel();
+    }
+
+    @Override
+    public void setWeaponLevel(int weaponLevel) {
+        iShip.setWeaponLevel(weaponLevel);
+
+    }
+
+
+
+
+
 
 
 }
